fd4c0192fb8372509c98389f2e90735a0ec012e6
josecovarrubias <josecovarrubias@unknown.domain>
2017-02-13 00:12:30 -0700

    mac set coll

--- TestingCollections/src/testingcollections/TestingCollections.java
+++ TestingCollections/src/testingcollections/TestingCollections.java
@@ -4,7 +4,10 @@
  */
 package testingcollections;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 /**@author josecovarrubias*/
 public class TestingCollections {    
     /**
@@ -23,34 +26,49 @@ public class TestingCollections {
         regArray[2]=103;
         regArray[3]=104;      
         /*This will print everything on the array. If I want to add more stuff
-        to this array I will have to modify the array's index number on line 16
+        to this array I will have to modify the array's index number on line 18
         and also add the new value to the actual array, or add a new method and
         receive new input from the user to be added to the array*/ 
         for(int i:regArray){
         System.out.print(i);
         System.out.println();
-        
-       
-    }
-       // this is just to separate the arrays 
-       System.out.println("================================================"
-                         +"\nNow Java collections which are similar to arrays"
-                         +"\n================================================");
-        
-       /*Different from regular arrays, collections can be a lot more simple.
-         
-       */
+    }//////////////////////////////////////////////////////////////////////////
+        /*              COLLECTION LIST         */
+       System.out.println("\n**Now Java collections**");       
+       /*Different from regular arrays, collections can be a lot more simple
+         and complex at the same time. For example a List can hava duplicates
+         of the data in it as long is the same type of data. 
+         It means that I can have 2 or more of the same thing, as shown  here.*/
         List stuff = new ArrayList();
         //data stored in the List collection.
         stuff.add("car1");
         stuff.add("car2");
-        stuff.add("car3");
+        stuff.add("car4");
         stuff.add("car4");
         //this displays the list.
-        System.out.println("**Display Stuff list**");
+        System.out.println();
         System.out.println(stuff);
-        
-        
+        ////////////////////////////////////////////////////////////////////////
+        /*              COLLECTION SET 
+        A collection Set can have different types of data as shown here,
+        but they cannot repeat. This could be helpful whe working with arrays
+        that require that data is enter only once. 
+        */
+        Set colSet = new HashSet();
+        colSet.add(2);
+        colSet.add("bike");
+        colSet.add(3.5);
+        System.out.println();
+        System.out.print(colSet);
+        System.out.print("\t #of index "+colSet.size()+"\n");
+        System.out.println();
+        System.out.println("**Displaying same colSet array with ITERATOR**");
+        Iterator colIterator = colSet.iterator();
+        while(colIterator.hasNext()){
+                Object data = colIterator.next();
+                System.out.print(data + ", ");
+        }
+        System.out.println();
     }
     
 }
